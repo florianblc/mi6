00fbd9d09e27357d38ef9157e3f8e3a06aba3837
tristanbsn <tristan.besson@wanadoo.fr>
2018-06-04 15:57:10 +0200

    Ajout adresse BDD.

--- Twitter/src/java/tier2/TwitterDBServiceClient.java
+++ Twitter/src/java/tier2/TwitterDBServiceClient.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tier2;
 
 import java.rmi.registry.*;
 import simo.mi6.project.tier3.TwitterDBService;
 
-/**
- *
- * @author trist
- */
 public class TwitterDBServiceClient {
 
+    // Retourne un objet TwitterDBService.
     public static TwitterDBService getTwitterDBServiceClient() throws Exception {
         System.setProperty("java.rmi.server.hostname", "127.0.0.1");
 
--- Twitter/src/java/tier2/tier1Interface.java
+++ Twitter/src/java/tier2/tier1Interface.java
@@ -5,7 +5,8 @@ import simo.mi6.project.tier3.TwitterDBService;
 public class tier1Interface {
 
     // Crée un nouvel utilisateur si username n'est pas déjà utilisé.
-    public void createUser(String username, String password) {
+    // Retourne true si l'utilisateur est créé, false dans le cas concert.
+    public boolean createUser(String username, String password) {
         try {
             // Initialisation de la connexion à la base de données.
             TwitterDBService twitterDB = TwitterDBServiceClient.getTwitterDBServiceClient();
@@ -15,12 +16,70 @@ public class tier1Interface {
                 System.out.println("L'utilisateur existe déjà.");
             } // Sinon...
             else {
+                // Ajout de l'utilisateur en base de données.
                 twitterDB.createNewUser(username, password);
                 System.out.println("L'utilisateur a été créé.");
+
+                return true;
             }
         } catch (Exception e) {
-
         }
+
+        return false;
+    }
+
+    // Supprime un utilisateur si username est présent en base de données.
+    // Return true si l'utilisateur est supprimé, false sinon.
+    public boolean deleteUser(String username) {
+        try {
+            // Initialisation de la connexion à la base de données.
+            TwitterDBService twitterDB = TwitterDBServiceClient.getTwitterDBServiceClient();
+
+            // Si username existe déjà en base de données...
+            if (twitterDB.getAllUsers().contains(username)) {
+                // Suppression de l'utilisateur de la base de données.
+                twitterDB.removeUser(username);
+                System.out.println("L'utilisateur a été supprimé.");
+
+                return true;
+            } // Sinon...
+            else {
+                System.out.println("L'utilisateur n'existe pas.");
+            }
+        } catch (Exception e) {
+        }
+
+        return false;
+    }
+
+    // Ajoute un nouveau tweet si username existe en base de données et que le tweet fait < de 280 charactères.
+    // Retourne true si le tweet est posté, false sinon.
+    public boolean createNewTweet(String username, String tweet) {
+        try {
+            // Initialisation de la connexion à la base de données.
+            TwitterDBService twitterDB = TwitterDBServiceClient.getTwitterDBServiceClient();
+
+            // Si username existe déjà en base de données...
+            if (twitterDB.getAllUsers().contains(username)) {
+                // Si le tweet fait moins de 280 charactères...
+                if (tweet.length() < 280) {
+                    // Ajout du tweet en base de données.
+                    twitterDB.createNewTweet(username, tweet);
+                    System.out.println("Le tweet a été créé.");
+
+                    return true;
+                } // Sinon..
+                else {
+                    System.out.println("Le tweet est trop long.");
+                }
+            } // Sinon...
+            else {
+                System.out.println("L'utilisateur n'existe pas.");
+            }
+        } catch (Exception e) {
+        }
+
+        return false;
     }
 
 }

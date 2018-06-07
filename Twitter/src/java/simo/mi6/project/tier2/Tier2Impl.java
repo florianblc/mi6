10e339934052154852b47d76dfa19c7208ee5158
tristanbsn <tristan.besson@wanadoo.fr>
2018-06-07 16:34:55 +0200

    Ajout de la fonction userConnexion et génération des fichiers de service CORBA pour tier2.

--- Twitter/src/java/simo/mi6/project/tier2/Tier2Impl.java
+++ Twitter/src/java/simo/mi6/project/tier2/Tier2Impl.java
@@ -4,7 +4,7 @@ import simo.mi6.project.tier3.TwitterDBService;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Tier2Interface {
+public class Tier2Impl extends Tier2POA {
 
     // Crée un nouvel utilisateur si username n'est pas déjà utilisé.
     // Retourne true si l'utilisateur est créé, false dans le cas concert.
@@ -54,6 +54,23 @@ public class Tier2Interface {
         return false;
     }
 
+    // Vérifier la correspondance entre le nom d'utilisateur et le mode passe.
+    // Retourne true si le couple nom d'utilisateur/Mot de passe est correcte, false sinon.
+    public boolean userConnexion(String username, String password) {
+        try {
+            // Initialisation de la connexion à la base de données.
+            TwitterDBService twitterDB = TwitterDBServiceClient.getTwitterDBServiceClient();
+
+            // Si le couple username/password est correcte...
+            if (twitterDB.isUserPasswordCorrect(username, password)) {
+                return true;
+            }
+        } catch (Exception e) {
+        }
+
+        return false;
+    }
+
     // Ajoute un nouveau tweet si username existe en base de données et que le tweet fait < de 280 charactères.
     // Retourne true si le tweet est posté, false sinon.
     public boolean createNewTweet(String username, String tweet) {
@@ -85,9 +102,10 @@ public class Tier2Interface {
     }
 
     // Retourne la liste des tweets à destination de l'utilisateur username.
-    public List<String> getTweetsForUser(String username) {
+    public String[] getTweetsForUser(String username) {
         // Initialisation de la liste des tweets à retourner.
-        List<String> tweets = new ArrayList<>();
+        ArrayList<String> tweetsList = new ArrayList<>();
+        String[] tweets;
 
         try {
             // Initialisation de la connexion à la base de données.
@@ -101,10 +119,16 @@ public class Tier2Interface {
                 // Pour chaques tweet de l'utilisateur suivis...
                 for (String tweet : twitterDB.getTweetsOfUser(user)) {
                     // Ajout du tweet à la liste des tweets retournés.
-                    tweets.add(tweet);
+                    tweetsList.add(tweet);
                 }
             }
         } catch (Exception e) {
+        }
+
+        // Remplissage de la liste des tweets à retourner.
+        tweets = new String[tweetsList.size()];
+        for (int i = 0; i < tweetsList.size(); i++) {
+            tweets[i] = tweetsList.get(i);
         }
 
         return tweets;
@@ -155,9 +179,10 @@ public class Tier2Interface {
     }
 
     // Retourne la liste des utilisateurs qui correspondent à la recherche.
-    public List<String> searchForUser(String searchUsername) {
+    public String[] searchForUser(String searchUsername) {
         // Liste des utilisateurs correspondant à la recherche.
-        ArrayList<String> matchingUsers = new ArrayList<>();
+        ArrayList<String> matchingUsersList = new ArrayList<>();
+        String[] matchingUser;
 
         try {
             // Initialisation de la connexion à la base de données.
@@ -169,18 +194,25 @@ public class Tier2Interface {
             // si la chaîne recherchée est contenu dans le nom d'utilisateur...
             users.stream().filter((user) -> (user.toUpperCase().contains(searchUsername.trim().toUpperCase()))).forEachOrdered((user) -> {
                 // Ajout de l'utilisateur à la liste des utilisateurs retournés.
-                matchingUsers.add(user);
+                matchingUsersList.add(user);
             });
         } catch (Exception e) {
         }
 
-        return matchingUsers;
+        // Remplissage de la liste d'utilisateurs à retourner.
+        matchingUser = new String[matchingUsersList.size()];
+        for (int i = 0; i < matchingUsersList.size(); i++) {
+            matchingUser[i] = matchingUsersList.get(i);
+        }
+
+        return matchingUser;
     }
 
     // Retourne la liste des tweets qui correspondent à la recherche.
-    public List<String> searchInTweets(String searchString) {
+    public String[] searchInTweets(String searchString) {
         // Liste des tweets correspondant à la recherche.
-        ArrayList<String> tweets = new ArrayList<>();
+        ArrayList<String> tweetsList = new ArrayList<>();
+        String[] tweets;
 
         try {
             // Initialisation de la connexion à la base de données.
@@ -193,11 +225,17 @@ public class Tier2Interface {
                     // Si la chaîne recherchée est contenu dans le tweet...
                     if (tweet.toUpperCase().contains(searchString.trim().toUpperCase())) {
                         // Ajout du tweet à la liste des tweets retournés.
-                        tweets.add(tweet);
+                        tweetsList.add(tweet);
                     }
                 }
             }
         } catch (Exception e) {
+        }
+
+        // Remplissage de la liste des tweets à retourner.
+        tweets = new String[tweetsList.size()];
+        for (int i = 0; i < tweetsList.size(); i++) {
+            tweets[i] = tweetsList.get(i);
         }
 
         return tweets;
